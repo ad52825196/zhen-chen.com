91675365438ea842842631ac468542aa75c94551
Zhen Chen <me@zhen-chen.com>
2016-04-23 16:29:01 +1200

    Home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layout')
 
+@section('keywords', '飞越彩虹, 新西兰, 奥克兰, 奥克兰大学, 上海交通大学, New Zealand, Auckland, University of Auckland, Shanghai Jiao Tong University, Computer Science, Information System, Zhen Chen')
+
+@section('title', 'Zhen Chen | 飞越彩虹')
+
+@section('canonical', 'https://zhen-chen.com/')
+
+@section('active', 'home')
+
 @section('head')
 <link rel="stylesheet" href="/css/cover.css" />
+@endsection
+
+@section('container')
+<div class="wrapper">
+    <h1>Kia Ora</h1>
+
+    <p class="lead">Greetings from New Zealand! My name is Zhen Chen (陈桢). I was a student in IEEE class at <a href="http://www.sjtu.edu.cn/">Shanghai Jiao Tong University</a>. Now I am studying Computer Science and Information System at <a href="https://www.auckland.ac.nz/en.html">The University of Auckland</a>. Currently, I am also a demonstrator for undergraduate labs in Computer Science department.</p>
+
+    <p class="lead">Feel free to <a href="about">contact me</a>, <a href="guestbook">say hello</a>, or read my <a href="//blog.zhen-chen.com/">blog</a>.</p>
+</div>
 @endsection
