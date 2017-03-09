456a8b4e1004383d11de1fffba84db1affbba5d8
Zhen Chen <me@zhen-chen.com>
2017-03-09 23:47:33 +1300

    Database for index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -21,10 +24,25 @@ class HomeController extends Controller
     }
 
     public function index() {
-        $data['keywords'] = 'Zhen Chen, 陈桢, 飞越彩虹';
+        $data['keywords'] = 'Zhen Chen, 陈桢, 飞越彩虹, Rainbow Studio, 彩虹工作室';
         $data['title'] = env('APP_NAME') . ' | 飞越彩虹';
         $data['canonical'] = env('APP_URL') . '/';
         $data['pageIdentifier'] = 'index';
+
+        // cache number of rows in table 'quote'
+        $minutes = 60;
+        $table = DB::table('quote');
+        $count = Cache::remember('quote.count', $minutes, function() use ($table) {
+            return $table -> count();
+        });
+        // generate random quote id of the day
+        $today = Carbon::today() -> timestamp;
+        srand($today);
+        $id = rand(1, $count);
+        // cache quote of the day
+        $data['quote'] = Cache::remember("quote.$id", Carbon::tomorrow(), function() use ($table, $id) {
+            return $table -> where('id', $id) -> value('quote');
+        });
 
         if ($this -> isAjax) {
             return $this -> handle('index', $data);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.home')
 
 @section('body')
-<div class="container">
+<div class="jumbotron center">
+    <div class="container">
+        <h1>"{{ $quote }}"</h1>
+    </div>
 </div>
 @stop
