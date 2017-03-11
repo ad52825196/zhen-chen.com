45c5a6948a13d5cf9534647278cf5390337f3afe
Zhen Chen <me@zhen-chen.com>
2017-03-12 11:59:22 +1300

    Update index wording

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'about' => 'About Me',
+    'about' => 'Me',
     'about_desc' => 'Hi! Welcome to my website. I am Zhen who is a slow learner, a passionate programmer, a lonely gamer, and an overwhelmed Computer Science student. Come if you want to know a little bit more about me.',
     'blog' => 'Blog',
     'blog_desc' => 'This is the place where I would like to share my life stories, technical tutorials, negligible thoughts, and maybe some interesting articles from the Internet.',
     'studio' => 'Rainbow Studio',
-    'studio_desc' => 'Sometimes, I need to design and develop some tools for my personal convenience but I am always happy to make them available to the public. Here you will find some web applications that I am working on.',
+    'studio_desc' => 'This is my personal studio. Here you can find some useful web applications, commercial products, and indie games that I am working on.',
     'go' => 'Go',
 ];
