cdce15a29b85b32b31d5c603052aeec056118d2a
Zhen Chen <me@zhen-chen.com>
2017-03-15 16:57:03 +1300

    Document on ready deprecated

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,14 @@ $('#to-top').affix({
 // leave space for footer
 $('body')[0].style.paddingBottom = $('footer').outerHeight(true) + 'px';
 
+// add target attribute to links pointing to other sites
+function linkTarget() {
+    $(document.links).filter(function() {
+        return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
+}
+$(linkTarget);
+
 // tooltip
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
@@ -29,18 +37,14 @@ $(function() {
 $(document).pjax('a:not([data-no-pjax])', '#body');
 $(document).on('pjax:start', function() { NProgress.start(); });
 $(document).on('pjax:end',   function() { NProgress.done();  });
-if ($(window).width() < 768) {
-    $(document).on('pjax:end', function() { $('.collapse').collapse('hide'); });
-}
+$(function() {
+    if ($(window).width() < 768) {
+        $(document).on('pjax:end', function() { $('.collapse').collapse('hide'); });
+    }
+});
+$(document).on('pjax:end', linkTarget);
 $(document).on('pjax:end', function() { ga('send', 'pageview'); });
 $(document).on('pjax:timeout', function(event) {
     // Prevent default timeout redirection behavior
     event.preventDefault();
-});
-
-// add target attribute to links pointing to other sites
-$(document).on('ready pjax:end', function() {
-    $(document.links).filter(function() {
-        return this.hostname != window.location.hostname;
-    }).attr('target', '_blank');
 });
