3969503efaf1f6907b40bdb0dd282382f7af6692
Zhen Chen <me@zhen-chen.com>
2017-03-28 21:16:50 +1300

    404 page multi language

---
+++ resources/lang/en/404.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'title' => 'Sorry, this is not the web page you are looking for.',
+    'home' => 'Home',
+];
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,8 +2,9 @@
 
 @section('body')
 <div class="container text-center">
-<h1>404 Sorry, this is not the web page you are looking for.</h1>
+<h1>404</h1>
+<h2>{{ __('404.title') }}</h2>
 <h2>{{ $exception -> getMessage() }}</h2>
-<p><a class="btn btn-success btn-lg" href="/" role="button">Home</a></p>
+<p><a class="btn btn-success btn-lg" href="/" role="button">{{ __('404.home') }}</a></p>
 </div>
 @stop
